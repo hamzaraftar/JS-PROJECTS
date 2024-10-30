ae9d7f8bece395521c9ab0373701453a7f71682d
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-30 02:26:29 -0700

    destructor of object

--- Es6/Destructuring Array/index.js
+++ Es6/Destructuring Array/index.js
@@ -1,4 +1,13 @@
 let user = ["hamza", 34];
 let = [name, age] = user;
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
+
+// --------------------Destructore of Objects----------------
+let user1 = {
+  name: "hamza",
+  age: 13,
+  city: "lahore",
+};
+let { city } = user1;
+console.log(city);
