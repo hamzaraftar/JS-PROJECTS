9871bcde7a0b8b40f9d4a4130e99f5e8b0fd737b
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-30 02:16:17 -0700

    destructor of array

---
+++ Es6/Destructuring Array/index.js
@@ -0,0 +1,4 @@
+let user = ["hamza", 34];
+let = [name, age] = user;
+console.log(name);
+console.log(age);
---
+++ Es6/Object Literals/index.js
@@ -0,0 +1,15 @@
+let name = "hamza";
+let course = "bs computer science";
+/*
+ let obj2 = {
+   name: name,
+   course: course,
+ };
+ console.log(obj2);
+*/
+// new way to create objects
+let obj2 = {
+  name,
+  course,
+};
+console.log(obj2);
