874798016d7d4f81a568ad416d2ac4c552c29bef
Hamza Asghar <hamzaraftar765@gmail.com>
2024-11-01 07:50:52 -0700

    list in python

---
+++ python/list.py
@@ -0,0 +1,13 @@
+# list are changeable
+a = [1,24,5,6,]
+a[0]=4
+# print(a)
+
+f1 = input('Enter Fruit Number 1  :')
+f2 = input('Enter Fruit Number 2  :')
+f3 = input('Enter Fruit Number 3  :')
+f4 = input('Enter Fruit Number 4  :')
+f5 = input('Enter Fruit Number 5  :')
+
+myFruits = [f1,f2,f3,f4,f5]
+print(myFruits)
